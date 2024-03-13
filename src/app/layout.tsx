8c9dc133e51a2589632e76d0826514b0db7f3a08
Jayant Malik <dev.jayantmalik@gmail.com>
2024-03-13 13:05:11 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "~/css/main.css";
+import { cn } from "~/lib/utils";
+import { ThemeProvider } from "~/components/theme-provider";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Picture Size Reducer | Develop Bharat",
+  description: "Reduce your photos size without uploading them online.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
